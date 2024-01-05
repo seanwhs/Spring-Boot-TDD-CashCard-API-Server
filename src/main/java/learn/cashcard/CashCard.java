80e15580b819a6fde9ed2a4b01d02e2f511e7bf3
Sean <seanwhs@gmail.com>
2024-01-05 18:16:39 +0800

    Initial Commit

---
+++ src/main/java/learn/cashcard/CashCard.java
@@ -0,0 +1,6 @@
+// CashCard.java
+package learn.cashcard;
+import org.springframework.data.annotation.Id;
+
+record CashCard(@Id Long id, Double amount) {
+}
---
+++ src/main/java/learn/cashcard/CashcardApplication.java
@@ -0,0 +1,13 @@
+package learn.cashcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class CashcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashcardApplication.class, args);
+	}
+
+}
